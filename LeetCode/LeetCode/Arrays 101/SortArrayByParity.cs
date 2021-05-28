0f0b8b887c0cdd6e964b159a03f683077830fb22
Тимур Сидорюк <skysidtim@gmail.com>
2021-05-28 17:59:30 +0700

    add sortByParity solution

---
+++ LeetCode/LeetCode/Arrays 101/SortArrayByParity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Arrays_101
+{
+    public class SortArrayByParity
+    {
+        public int[] Solution(int[] nums)
+        {
+            var result = new int[nums.Length];
+            var oddNums = new List<int>();
+            var resultCounter = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    result[resultCounter] = nums[i];
+                    resultCounter++;
+                }
+                else
+                {
+                    oddNums.Add(nums[i]);
+                }
+            }
+
+            for (var i = 0; i < oddNums.Count; i++)
+            {
+                result[resultCounter] = oddNums[i];
+                resultCounter++;
+            }
+
+            return result;
+        }
+    }
+}
--- LeetCode/Tests/DataStructures/Arrays101.cs
+++ LeetCode/Tests/DataStructures/Arrays101.cs
@@ -7,17 +7,15 @@ namespace Tests.DataStructures
     [TestFixture]
     public class Tests
     {
-
-        [TestCase(new[]{1,1,1,0}, ExpectedResult = 3)]
-        [TestCase(new[]{0,0,0,0}, ExpectedResult = 0)]
-        [TestCase(new[]{1,0,0,0}, ExpectedResult = 1)]
-        [TestCase(new[]{0,1,0,0}, ExpectedResult = 1)]
-        [TestCase(new[]{0,0,1,0}, ExpectedResult = 1)]
-        [TestCase(new[]{1,1,0,1}, ExpectedResult = 2)]
-        [TestCase(new[]{1,0,1,0}, ExpectedResult = 1)]
-        [TestCase(new[]{0,1,1,0}, ExpectedResult = 2)]
-        [TestCase(new[]{0,1,1,0,1,1,1}, ExpectedResult =3)]
-
+        [TestCase(new[] { 1, 1, 1, 0 }, ExpectedResult = 3)]
+        [TestCase(new[] { 0, 0, 0, 0 }, ExpectedResult = 0)]
+        [TestCase(new[] { 1, 0, 0, 0 }, ExpectedResult = 1)]
+        [TestCase(new[] { 0, 1, 0, 0 }, ExpectedResult = 1)]
+        [TestCase(new[] { 0, 0, 1, 0 }, ExpectedResult = 1)]
+        [TestCase(new[] { 1, 1, 0, 1 }, ExpectedResult = 2)]
+        [TestCase(new[] { 1, 0, 1, 0 }, ExpectedResult = 1)]
+        [TestCase(new[] { 0, 1, 1, 0 }, ExpectedResult = 2)]
+        [TestCase(new[] { 0, 1, 1, 0, 1, 1, 1 }, ExpectedResult = 3)]
         public int MaxConsequenceTest(int[] data)
         {
             var testClass = new FindMaxConsecutiveOnes();
@@ -25,24 +23,24 @@ namespace Tests.DataStructures
             return res;
         }
 
-        [TestCase(new[]{1,0,2,3,0,4,5,0}, ExpectedResult = new[]{1,0,0,2,3,0,0,4})]
-        [TestCase(new[]{1,0,2,0,0,4,5,0}, ExpectedResult = new[]{1,0,0,2,0,0,0,0})]
+        [TestCase(new[] { 1, 0, 2, 3, 0, 4, 5, 0 }, ExpectedResult = new[] { 1, 0, 0, 2, 3, 0, 0, 4 })]
+        [TestCase(new[] { 1, 0, 2, 0, 0, 4, 5, 0 }, ExpectedResult = new[] { 1, 0, 0, 2, 0, 0, 0, 0 })]
         public int[] DuplicateZerosTestSolution1(int[] data)
         {
             var testClass = new DuplicateZeros();
             var res = testClass.Solution1(data);
             return res;
         }
-        
-        [TestCase(new[]{1,0,2,3,0,4,5,0}, ExpectedResult = new[]{1,0,0,2,3,0,0,4})]
-        [TestCase(new[]{1,0,2,0,0,4,5,0}, ExpectedResult = new[]{1,0,0,2,0,0,0,0})]
+
+        [TestCase(new[] { 1, 0, 2, 3, 0, 4, 5, 0 }, ExpectedResult = new[] { 1, 0, 0, 2, 3, 0, 0, 4 })]
+        [TestCase(new[] { 1, 0, 2, 0, 0, 4, 5, 0 }, ExpectedResult = new[] { 1, 0, 0, 2, 0, 0, 0, 0 })]
         public int[] DuplicateZerosTestSolution2(int[] data)
         {
             var testClass = new DuplicateZeros();
             var res = testClass.Solution2(data);
             return res;
         }
-        
+
         [Test]
         public void MergeSortedArraySolution1()
         {
@@ -55,7 +53,7 @@ namespace Tests.DataStructures
             var res = testClass.Solution1(nums1, m, nums2, n);
             Assert.AreEqual(expectedResult, res);
         }
-        
+
         [Test]
         public void MergeSortedArraySolution2()
         {
@@ -72,7 +70,7 @@ namespace Tests.DataStructures
         [Test]
         public void RemoveElementSolution()
         {
-            var nums = new[] {3,2,2,3 };
+            var nums = new[] { 3, 2, 2, 3 };
             var val = 3;
             var testClass = new RemoveElement();
             var res = testClass.Solution(nums, val);
@@ -83,47 +81,58 @@ namespace Tests.DataStructures
         [Test]
         public void RemoveDuplicatesSolution()
         {
-            var nums = new[] {0,0,1,1,1,2,2,3,3,4};
+            var nums = new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
 
-        var testClass = new RemoveDuplicates();
+            var testClass = new RemoveDuplicates();
             var res = testClass.Solution(nums);
             Assert.AreEqual(5, res);
         }
-        
-        [TestCase(new[]{3,1,7,11}, ExpectedResult = false)]
-        [TestCase(new[]{7,1,14,11}, ExpectedResult = true)]
+
+        [TestCase(new[] { 3, 1, 7, 11 }, ExpectedResult = false)]
+        [TestCase(new[] { 7, 1, 14, 11 }, ExpectedResult = true)]
         public bool CheckIfExistSolution(int[] data)
         {
             var testClass = new CheckIfNandItsDoubleExist();
             return testClass.Solution(data);
         }
-        
-        [TestCase(new[]{2,1}, ExpectedResult = false)]
-        [TestCase(new[]{3,5,5}, ExpectedResult = false)]
-        [TestCase(new[]{0,3,2,1}, ExpectedResult = true)]
+
+        [TestCase(new[] { 2, 1 }, ExpectedResult = false)]
+        [TestCase(new[] { 3, 5, 5 }, ExpectedResult = false)]
+        [TestCase(new[] { 0, 3, 2, 1 }, ExpectedResult = true)]
         public bool ValidMountainArraySolution(int[] data)
         {
             var testClass = new ValidMountainArray();
             return testClass.Solution(data);
         }
-        
-        [TestCase(new[]{17,18,5,4,6,1}, ExpectedResult = new[]{18,6,6,6,1,-1})]
-        [TestCase(new[]{400}, ExpectedResult = new[]{-1})]
+
+        [TestCase(new[] { 17, 18, 5, 4, 6, 1 }, ExpectedResult = new[] { 18, 6, 6, 6, 1, -1 })]
+        [TestCase(new[] { 400 }, ExpectedResult = new[] { -1 })]
         public int[] ReplaceElementsSolution(int[] data)
         {
             var testClass = new ReplaceElements();
             return testClass.Solution(data);
         }
-        
+
         [Test]
         public void MoveZeroesSolution()
         {
-            var nums = new[] {0,1,0,3,12};
+            var nums = new[] { 0, 1, 0, 3, 12 };
             var res = new[] { 1, 3, 12, 0, 0 };
 
             var testClass = new MoveZeroes();
             testClass.Solution(nums);
             Assert.AreEqual(nums, res);
+        }
+
+        [Test]
+        public void SortArrayByParitySolution()
+        {
+            var nums = new[] { 3, 1, 2, 4 };
+            var expected = new[] { 2, 4, 3, 1 };
+
+            var testClass = new SortArrayByParity();
+            var res = testClass.Solution(nums);
+            Assert.AreEqual(expected, res);
         }
     }
 }
