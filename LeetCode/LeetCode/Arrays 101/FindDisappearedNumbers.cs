c9f3804b7a25681212fa289b25dcbfff0ea49bc9
Тимур Сидорюк <skysidtim@gmail.com>
2021-06-22 17:45:21 +0700

    Add find FindDisappearedNumbers

---
+++ LeetCode/LeetCode/Arrays 101/FindDisappearedNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Arrays_101
+{
+    public class FindDisappearedNumbers
+    {
+        public IList<int> Solution(int[] nums)
+        {
+            for (var i = 0; i < nums.Length; i ++) {
+                var index = Math.Abs(nums[i]) - 1;
+                if (nums[index] > 0) {
+                    nums[index] = - nums[index];
+                }        
+            }
+            
+            var result = new List<int>();
+            
+            for (var i = 0; i < nums.Length; i ++) {
+                if (nums[i] > 0) {
+                    result.Add(i + 1);
+                }
+            }
+            
+            return result;
+        }
+    }
+}
