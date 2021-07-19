da2adb386913048d5ae26f1931e051da65ec85cf
Тимур Сидорюк <skysidtim@gmail.com>
2021-07-19 15:05:57 +0700

    add reverse string solution

---
+++ LeetCode/LeetCode/Recursion/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Recursion
+{
+    public class ReverseString
+    {
+        public char[] Solution(char[] s)
+        {
+            var topBorder = s.Length - 1;
+            for (var i = 0; i < topBorder; i++)
+            {
+                var temp = s[i];
+                s[i] = s[topBorder];
+                s[topBorder] = temp;
+
+                topBorder--;
+            }
+
+            return s;
+        }
+    }
+}
---
+++ LeetCode/Tests/Recursion/RecursionTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Recursion;
+
+using NUnit.Framework;
+
+namespace Tests.Recursion
+{
+    [TestFixture]
+    public class RecursionTests
+    {
+        [TestCase(new[] {'h','e','l','l','o' }, ExpectedResult = new[] {'o','l','l','e','h' })]
+        [TestCase(new[] {'H','a','n','n','a','h' }, ExpectedResult = new[] {'h','a','n','n','a','H'})]
+        public char[] ReverseString (char[] data)
+        {
+            var testClass = new ReverseString();
+            var res = testClass.Solution(data);
+            return res;
+        }
+    }
+}
