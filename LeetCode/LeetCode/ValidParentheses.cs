54935b3a12003e412865e63f0779b001ab6483f7
Тимур Сидорюк <skysidtim@gmail.com>
2021-09-06 15:44:39 +0700

    Add valid parentheses solution

---
+++ LeetCode/LeetCode/ValidParentheses.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class ValidParentheses
+    {
+        public bool Solution(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var t in s)
+            {
+                if(t == '(')
+                    stack.Push(')');
+                else if(t == '{')
+                    stack.Push('}');
+                else if(t == '[')
+                    stack.Push(']');
+                else if (stack.Count == 0 || stack.Pop() != t)
+                    return false;
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
