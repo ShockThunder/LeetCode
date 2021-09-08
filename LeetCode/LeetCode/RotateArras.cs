c75354da194e9f1d5e4fac636ac37fd9b280c055
Тимур Сидорюк <skysidtim@gmail.com>
2021-09-08 14:05:28 +0700

    Add day2 "algorithm I" solution

---
+++ LeetCode/LeetCode/RotateArras.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode
+{
+    public class RotateArray
+    {
+        public void Solution(int[] nums, int k) 
+        {
+            if (nums.Length < 2)
+                return;
+            
+            if(k == 0)
+                return;
+
+            while (k > nums.Length)
+            {
+                k -= nums.Length;
+            }
+
+            var rotatedTail = new int[k];
+            
+            //fill tail values
+            for (var i = 0; i < k; i++)
+            {
+                rotatedTail[^(i + 1)] = nums[^(i + 1)];
+            }
+
+            for (var i = nums.Length - 1; i >= k; i--)
+            {
+                nums[i] = nums[i - k];
+            }
+
+            for (var i = 0; i < k; i++)
+            {
+                nums[i] = rotatedTail[i];
+            }
+        }
+    }
+}
---
+++ LeetCode/Tests/MediumTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode;
+
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class MediumTests
+    {
+        [Test]
+        public void RotateArrayTests()
+        {
+            var nums = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var k = 3;
+            var solver = new RotateArray();
+            solver.Solution(nums, k);
+            Assert.AreEqual(nums, new []{5,6,7,1,2,3,4});
+        }
+    }
+}
