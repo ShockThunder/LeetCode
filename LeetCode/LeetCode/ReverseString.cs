b1ec7187bffb2c30571d42a0adfb84209f784933
Тимур Сидорюк <skysidtim@gmail.com>
2021-09-10 13:41:17 +0700

    add ReverseString and reverse words string

---
+++ LeetCode/LeetCode/ReverseString.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class ReverseString
+    {
+        public void Solution(char[] s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+
+            while (left < right)
+            {
+                var temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode/ReverseWordInAString.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+    public class ReverseWordInAString
+    {
+        public string Solution(string s)
+        {
+            var words = s.Split(' ');
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                var word = words[i].ToCharArray();
+
+                for (var j = 0; j < word.Length / 2; j++)
+                {
+                    var temp = word[j];
+                    word[j] = word[word.Length - 1 - j];
+                    word[word.Length - 1 - j] = temp;
+                }
+
+                words[i] = new string(word);
+            }
+
+            var result = string.Empty;
+            for (var i = 0; i < words.Length; i++)
+            {
+                result += words[i] + " ";
+            }
+
+            result = result.Trim();
+
+            return result;
+        }
+    }
+}
