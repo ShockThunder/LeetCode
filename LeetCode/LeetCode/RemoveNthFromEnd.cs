8fc20ac3f5a087a564852eaa603ef3174d76cb6c
Тимур Сидорюк <Sidoryk_TY@nposapfir.ru>
2021-09-14 12:00:04 +0700

    add some nodes solutions

---
+++ LeetCode/LeetCode/RemoveNthFromEnd.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class RemoveNthFromEnd
+    {
+        public ListNode Solution(ListNode head, int n)
+        {
+            var left = head;
+            var right = head;
+
+            while(right != null) {
+                right = right.next;
+                if (n-- < 0) left = left.next; 
+            } 
+
+            if (n == 0) head = head.next;
+            else left.next = left.next.next;
+
+            return head;        
+        }
+    }
+}
